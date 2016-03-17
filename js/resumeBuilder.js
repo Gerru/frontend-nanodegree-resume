376b7e995a49bceaeda01ffbaabd362d904be8b5
Gerru Kloppers <Gerru.Kloppers@training.futuretrainings.com>
2016-03-17 09:01:22 +0100

    added data to resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,50 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// $("#main").append('Gerru Kloppers');
+var name = "Gerru Kloppers";
+var role = "Data Analyst";
+
+
+var fname = HTMLheaderName.replace("%data%", name);
+var frole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(frole);
+$("#header").prepend(fname);
+
+var bio = {
+    "name": "Gerru Kloppers",
+    "role": "Data Analyst",
+    "contacs": {
+        "email": "gkloppers@gmail.com",
+        "city": "Berlin",
+        "phone": "0123455676789"
+    },
+    "mypic": "images/fry.jpg",
+    "skills": ["1", "2", "3"]
+};
+
+var work = {
+    "employer": "idealo",
+    "position": "Content Manager"
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "Univarsity of Pretoria",
+            "city": "Pretoria",
+            "course": "B.IS. Information and Knowledge Management"
+        },
+        {
+            "name": "Coursera",
+            "city": "Online",
+            "course": "Data Science"            
+        }
+        ]
+    
+}
+
+//$("#main").append(bio.contacs.city);
+//$("#main").append(bio.work.position);
+//$("#main").append(bio.education);
